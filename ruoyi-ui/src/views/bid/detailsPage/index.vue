406dc20eab689f5729221b3502a99e37c0a34927
曹浩博 <2278686667@qq.com>
2023-04-14 17:36:07 +0800

    评分

---
+++ ruoyi-ui/src/views/bid/detailsPage/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <p v-html="data.remark" style="margin-left: 20px"></p>
+
+  </div>
+</template>
+
+<script>
+import {
+
+  getInvite_tenders,
+
+} from "@/api/bid/invite_tenders";
+
+export default {
+  name: "index",
+  data() {
+    return {
+      data: ""
+    }
+  },
+  created() {
+    getInvite_tenders(this.$route.query.id).then(res => {
+      this.data = res.data;
+      console.log(this.data)
+
+    })
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ ruoyi-ui/src/views/bid/projStatus/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <el-steps :active="number" simple>
+      <el-step title="待发售" icon="el-icon-edit">111</el-step>
+
+      <el-step title="发售中" icon="el-icon-upload"></el-step>
+      <el-step title="开标中" icon="el-icon-s-flag"></el-step>
+      <br/>
+      <el-step title="待评标" icon="el-icon-s-flag"></el-step>
+      <el-step title="评审中" icon="el-icon-s-flag"></el-step>
+      <el-step title="评审完成" icon="el-icon-s-flag"></el-step>
+      <el-step title="已完成" icon="el-icon-s-flag"></el-step>
+      <el-step title="已终止" icon="el-icon-s-flag"></el-step>
+    </el-steps>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "index",
+  data() {
+    return {
+      id: null,
+      number: "0"
+    }
+  },
+  created() {
+    console.log(this.$route.query.id)
+    this.id = this.$route.query.id;
+    this.number = parseInt(this.id);
+  }
+}
+</script>
+
+<style scoped>
+.el-steps {
+  margin-top: 100px;
+}
+</style>
